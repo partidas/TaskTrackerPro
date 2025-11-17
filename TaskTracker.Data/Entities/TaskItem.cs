d231df5f06cc1be5efbc111bccab04db619b9ca6
Steven Partida <ste94par@gmail.com>
2025-11-16 21:58:36 -0800

    Third commit

--- TaskTracker.Data/Entities/TaskItem.cs
+++ TaskTracker.Data/Entities/TaskItem.cs
@@ -1,1 +1,12 @@
 // TaskItem.cs defines the TaskItem mode
+namespace TaskTracker.Data.Entities;
+
+public class User
+{
+    public int Id { get; set; }     // Primary Key
+    public string Username {get; set; } = ""; // Unique username
+    public string PasswordHash { get; set; } = ""; // Hashed password
+
+    // Navigation property: one user can have many tickets
+    public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+}
